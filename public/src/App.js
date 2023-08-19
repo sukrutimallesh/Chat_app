d2aae3823b89be72c302bf275d8d396d225862ca
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-08-19 15:58:13 -0700

    route setup done

--- public/src/App.js
+++ public/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
+export default function App() {
   return (
-    <div>App</div>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route path = '/register' element = {<Register/>}/>
+      <Route path = '/login' element = {<Login/>}/>
+      <Route path = '/chat' element = {<Chat/>}/>
+    </Routes>
+  </BrowserRouter>
+  );
 }
 
-export default App
