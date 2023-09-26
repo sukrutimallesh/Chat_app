a67c2985ee39b5628b97f8e4ac1bd0d2eb49d752
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-09-25 19:16:39 -0700

    register and login done

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', userRoutes)
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
