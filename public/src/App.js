fec0f495ddc2e11f9f08347ff5dd72f14672fb01
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-10-12 16:19:57 -0700

    final changes

--- public/src/App.js
+++ public/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import SetAvatar from './componenets/SetAvatar'
+import SetAvatar from './components/SetAvatar'
 
 export default function App() {
   return (
